c32f2f3999c43c7e217e3da33f8bb3a2e3eaf513
FletisZi <rjrodrigosantos12@gmail.com>
2023-08-30 09:40:32 -0300

    channel image update

--- apiT/server.js
+++ apiT/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const app = express();
 
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'https://www.twitch.tv/japagameplay11',
+  'headers': {
+    'Client-Id': 'jf3m5uz1o2zpd8qpf5xgjoshstmk0w',
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+//   console.log(response.headers)
+  app.get('/teste',function(req,res){
+        res.send(response.body)
+  })
+});
 
 
 
@@ -9,6 +24,9 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html"); 
     
 })
+
+
+
 
 
 
